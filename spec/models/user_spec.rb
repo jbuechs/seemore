8c7d9284a050d53b0c4090b67d761165f3291fc1
Jennie Buechner <janina.buechner@gmail.com>
2016-01-07 11:03:30 -0800

    add test for user model validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:uid) }
+  it { is_expected.to validate_presence_of(:provider) }
 end
