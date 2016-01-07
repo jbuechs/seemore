33b18fcb9372e3a731cf51d22d0b40f811d4cfc4
Claire <ClaireS29@gmail.com>
2016-01-07 14:21:50 -0800

    finished developer solutions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  skip_before_filter :verify_authenticity_token
+  skip_before_filter :verify_authenticity_token, only: :create
 
   def create
     #can sign in with twitter or vimeo
