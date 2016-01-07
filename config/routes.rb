869b6a1acb5e51869e36da2dbaad4c6c25975ad8
Desiree <desireepoland@gmail.com>
2016-01-07 11:49:16 -0800

    remove redundant routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
-  get 'users/show'
-
-  get 'users/feed'
-
-  get 'users/delete'
-
-  get 'users/create'
-
   root 'users#feed'
+  
   get "/auth/:provider/callback" => "sessions#create"
   get "/login" => "sessions#new", as: :login
   delete "/logout/"              => "sessions#destroy", as: :logout
