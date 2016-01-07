96c1bb341d8b85117b047805f804f2b60f9fc63c
Claire <ClaireS29@gmail.com>
2016-01-07 14:02:45 -0800

    debugging

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :developer if Rails.env.development?
+  provider :developer,
+    :fields => [:name, :uid] if Rails.env.development?
   provider :vimeo, ENV['VIMEO_KEY'], ENV['VIMEO_SECRET']
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
 end
