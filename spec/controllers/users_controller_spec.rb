b78b948f24985fb9f6f334f36fb8d8b61c83b065
Jennie Buechner <janina.buechner@gmail.com>
2016-01-07 14:37:53 -0800

    remove extra users controller rspec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,32 +2,20 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #feed" do
-    it "returns http success" do
-      get :feed
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #delete" do
-    it "returns http success" do
-      get :delete
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET #show" do
+  #
+  # end
+  #
+  # describe "GET #feed" do
+  #
+  # end
+  #
+  # describe "GET #delete" do
+  #
+  # end
+  #
+  # describe "GET #create" do
+  #
+  # end
 
 end
