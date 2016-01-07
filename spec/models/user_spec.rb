fd579e9684cff0f25f2675f4c1ad3d504c43ff97
Jennie Buechner <janina.buechner@gmail.com>
2016-01-07 11:10:08 -0800

    complete user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_presence_of(:uid) }
   it { is_expected.to validate_presence_of(:provider) }
+
+  describe ".initialize_from_omniauth" do
+    let(:user) { User.find_or_create_from_omniauth(OmniAuth.config.mock_auth[:vimeo]) }
+
+    it "creates a valid user" do
+      expect(user).to be_valid
+    end
+
+    context "when it's invalid" do
+      it "returns nil" do
+        user = User.find_or_create_from_omniauth({"uid" => "123", "info" => {}})
+        expect(user).to be_nil
+      end
+    end
+
+    context "when the user has already logged in" do
+      it "returns user" do
+        user
+        user = User.find_or_create_from_omniauth(OmniAuth.config.mock_auth[:vimeo])
+        expect(user).to be_an_instance_of(User)
+      end
+    end
+  end
 end
