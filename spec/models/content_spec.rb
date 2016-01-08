6063aa7a02c22ad50c9fe586fb2d7ca1e6f2c81e
Jennie Buechner <janina.buechner@gmail.com>
2016-01-08 15:38:28 -0800

    add validation tests for content model

--- spec/models/content_spec.rb
+++ spec/models/content_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Content, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:content_id) }
+  it { is_expected.to validate_presence_of(:text) }
+  it { is_expected.to validate_presence_of(:create_time) }
+  it { is_expected.to validate_presence_of(:favorites) }
+  it { is_expected.to validate_presence_of(:embed_code) }
+  it { is_expected.to validate_presence_of(:creator_id) }
 end
