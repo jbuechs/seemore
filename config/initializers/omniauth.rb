c3316b34d1269036a91fbc7048d5bc3859a9ca0e
Jennie Buechner <janina.buechner@gmail.com>
2016-01-08 14:07:52 -0800

    bugfix developer mode login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :developer, 
-    :fields => [:name, :uid] if Rails.env.development?
+  provider :developer,
+    :fields => [:name, :uid],
+    :uid_field => :uid if Rails.env.development?
   provider :vimeo, ENV['VIMEO_KEY'], ENV['VIMEO_SECRET']
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
 end
