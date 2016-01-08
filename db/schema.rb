b25289a2092601dc9c2270313f614a3a1baa3ab4
Claire <ClaireS29@gmail.com>
2016-01-08 14:57:54 -0800

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160107184005) do
+ActiveRecord::Schema.define(version: 20160108225319) do
+
+  create_table "creators", force: :cascade do |t|
+    t.integer  "p_id"
+    t.string   "provider"
+    t.string   "avatar_url"
+    t.string   "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "uid"
