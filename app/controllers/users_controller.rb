e9278e7b3fd649da88f1fef6aedb540f78f48b56
Jennie Buechner <janina.buechner@gmail.com>
2016-01-11 11:39:32 -0800

    create update method to add creator to user's list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,4 +13,23 @@ class UsersController < ApplicationController
 
   def create
   end
+
+  # adds a creator to the user's list
+  def update
+    # if the creator already exists, get the creator and add to user's list
+    # if the creator does not exist, create creator and add to user's list
+    creator = Creator.create(creator_hash(params))
+    current_user.creators << creator
+    raise
+  end
+
+  private
+  def creator_hash(params)
+    creator_hash = {}
+    creator_hash[:p_id] = params["p_id"]
+    creator_hash[:username] = params["username"]
+    creator_hash[:avatar_url] = params["avatar_url"]
+    creator_hash[:provider] = params["provider"]
+    return creator_hash
+  end
 end
