b05ba4796bd0745ce9329452f97caacab9c81256
Desiree <desireepoland@gmail.com>
2016-01-11 09:43:32 -0800

    Add has_image method to creator model

--- app/models/creator.rb
+++ app/models/creator.rb
@@ -1,4 +1,12 @@
 class Creator < ActiveRecord::Base
   has_many :content
   validates :p_id, :provider, :username, presence: true
+
+  def has_image?
+    if avatar_url.nil?
+      false
+    else
+      true
+    end
+  end
 end
