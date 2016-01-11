d164d494e8a99ea72eb37aecfbde4e3e7544933a
Desiree <desireepoland@gmail.com>
2016-01-11 10:14:38 -0800

    re add jennies code for vimeo_search

--- app/controllers/creators_controller.rb
+++ app/controllers/creators_controller.rb
@@ -31,24 +31,23 @@ class CreatorsController < ApplicationController
   end
 
   private
-    def vimeo_search(query)
-      response = HTTParty.get("https://api.vimeo.com/users?per_page=#{LIMIT_PER_PAGE}&query=#{query}", headers: {"Authorization" => "bearer #{ENV['VIMEO_ACCESS_TOKEN']}"})
-      json_res = convert_to_json(response)
-      vim_users = json_res["data"]
-      videographers = []
-      vim_users.each do |user|
-        # create user objects
-        videographer = Videographer.new
-        # populate videographer fields
-        # videographer.uid = get_vimeo_id(user["uri"])
-        # videographer.name = user["name"]
-        # and so on
-        # purposely do not save the videographer objects to the database,
-        # as this is only for displaying
-        videographers << videographer
-      end
-      return videographers
-    end
+  def vimeo_search(query)
+     response = HTTParty.get("https://api.vimeo.com/users?per_page=#{LIMIT_PER_PAGE}&query=#{query}", headers: {"Authorization" => "bearer #{ENV['VIMEO_ACCESS_TOKEN']}"})
+     json_res = convert_to_json(response)
+     vim_users = json_res["data"]
+     videographers = []
+     vim_users.each do |user|
+       videographer = Creator.new
+       videographer.p_id = get_vimeo_id(user["uri"])
+       videographer.provider = "vimeo"
+       videographer.username = user["name"]
+       videographer.avatar_url = user["pictures"]["sizes"][2]["link"] if user["pictures"]
+       # purposely do not save the videographer objects to the database,
+       # as this is only for displaying
+       videographers << videographer
+     end
+     return videographers
+   end
 
     def convert_to_json(httparty_response)
       if httparty_response.class == HTTParty::Response
