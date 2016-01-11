a08734b2b3433e4cfa232e8d1bc4727369a2834b
Jennie Buechner <janina.buechner@gmail.com>
2016-01-11 13:36:40 -0800

    create test for creator.has_image? method

--- spec/models/creator_spec.rb
+++ spec/models/creator_spec.rb
@@ -5,4 +5,14 @@ RSpec.describe Creator, type: :model do
   it { is_expected.to validate_presence_of(:provider) }
   it { is_expected.to validate_presence_of(:username) }
 
+  describe "has_image?" do
+    let(:creator) { FactoryGirl.create(:creator) }
+    it "returns true when creator object has image" do
+      creator.avatar_url = "abc"
+      expect(creator.has_image?).to be true
+    end
+    it "returns false when creator object has no image" do
+      expect(creator.has_image?).to be false
+    end
+  end
 end
