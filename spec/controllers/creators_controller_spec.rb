3ee2b5807d4f593619a0572dbeaaf54779e56e87
Jennie Buechner <janina.buechner@gmail.com>
2016-01-12 14:50:24 -0800

    start spec

--- spec/controllers/creators_controller_spec.rb
+++ spec/controllers/creators_controller_spec.rb
@@ -1,18 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe CreatorsController, type: :controller do
-  # 
-  # def "search" do
-  #   context "vimeo query" do
-  #     it "returns an array of Videographers" do
-  #
-  #     end
-  #   end
-  #   context "twitter query" do
-  #     it "returns an array of Tweeters" do
-  #
-  #     end
-  #   end
-  # end
+
+  describe "search" do
+    let (:vimeo_search_params) {
+      { params: { vimeoquery: "Hello" } }
+    }
+    let (:twitter_search_params) {
+      { params: { twitterquery: "Hello" } }
+    }
+    let (:invalid_search_params) {
+      { params: { twitterquery: "" } }
+    }
+    context "vimeo query" do
+      it "redirects to search page" do
+        get :search, vimeo_search_params
+        expect(subject).to redirect_to :search
+
+      end
+    end
+    # context "twitter query" do
+    #   it "returns an array of Creators from twitter" do
+    #
+    #   end
+    # end
+  end
 
 end
