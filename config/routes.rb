d0ef1506571d3be35d7cae6c2205e813453b6eef
Desiree <desireepoland@gmail.com>
2016-01-12 19:52:55 -0800

    Add a route to delete a creator from a user

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   get "/login" => "sessions#new", as: :login
   delete "/logout/"              => "sessions#destroy", as: :logout
   get "/search/"                => "creators#search"
-  resources :users, only: [:show, :delete, :create, :update]
+  resources :users, only: [:show, :delete, :create, :update] do
+    get 'delete_creator', on: :member   # with member, the url looks like: /users/:id/delete_creator
+  end
 
 
 end
