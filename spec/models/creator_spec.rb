eecdfad4e79aba0d4a758faf096b3410e6e0ae02
Desiree <desireepoland@gmail.com>
2016-01-14 09:40:00 -0800

    add vimeo creator contet to has_image spec

--- spec/models/creator_spec.rb
+++ spec/models/creator_spec.rb
@@ -6,13 +6,25 @@ RSpec.describe Creator, type: :model do
   it { is_expected.to validate_presence_of(:username) }
 
   describe "has_image?" do
-    let(:creator) { build(:twitter_creator) }
-    it "returns true when creator object has image" do
-      creator.avatar_url = "abc"
-      expect(creator.has_image?).to be true
+    context "creator is from twitter" do
+      let(:creator) { build(:twitter_creator) }
+      it "returns true when creator object has image" do
+        creator.avatar_url = "abc"
+        expect(creator.has_image?).to be true
+      end
+      it "returns false when creator object has no image" do
+        expect(creator.has_image?).to be false
+      end
     end
-    it "returns false when creator object has no image" do
-      expect(creator.has_image?).to be false
+    context "creator is from vimeo" do
+      let(:creator) { build(:vimeo_creator) }
+      it "returns true when creator object has image" do
+        creator.avatar_url = "abc"
+        expect(creator.has_image?).to be true
+      end
+      it "returns false when creator object has no image" do
+        expect(creator.has_image?).to be false
+      end
     end
   end
 
