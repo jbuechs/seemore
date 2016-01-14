31d3ea709dc288c31015a126551a2a51c1273fb4
Jennie Buechner <janina.buechner@gmail.com>
2016-01-14 10:26:45 -0800

    fix creators controller spec

--- spec/controllers/creators_controller_spec.rb
+++ spec/controllers/creators_controller_spec.rb
@@ -15,6 +15,8 @@ RSpec.describe CreatorsController, type: :controller do
     let (:invalid_search_params) {
       { query: "" }
     }
+    let (:user) { create(:user) }
+
     context "blank search" do
       it "creates a flash message" do
         get :search, invalid_search_params
@@ -22,23 +24,21 @@ RSpec.describe CreatorsController, type: :controller do
       end
 
       it "redirects to root_path" do
-        @current_user = create(:user)
-        session[:user_id] = @current_user.id
-        get :search, invalid_search_params
+        get :search, invalid_search_params, user_id: user.id
         expect(subject).to redirect_to root_path
       end
     end
 
     context "vimeo query" do
       it "redirects to search page" do
-        get :search, vimeo_search_params
+        get :search, vimeo_search_params, user_id: user.id
         expect(subject).to render_template :search
       end
     end
 
     context "twitter query" do
       it "returns an array of Creators from twitter" do
-        get :search, twitter_search_params
+        get :search, twitter_search_params, user_id: user.id
         expect(subject).to render_template :search
       end
     end
